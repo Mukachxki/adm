75c0f81bc447d007b970557ba798892b9edbee06
Mukachxki <aaronfernandez383@gmail.com>
2024-12-09 20:13:20 +0800

    +1

---
+++ app/(tabs)/buttons/rating.tsx
@@ -0,0 +1,11 @@
+// app/(tabs)/index.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function RateTab() {
+  return (
+    <View>
+      <Text>Rating Tab Screen</Text>
+    </View>
+  );
+}
